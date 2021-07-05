8375ca59f4f27bf590534890bd7c3780cf57fa98
Miguel <miguelmgonzalez7@gmail.com>
2021-07-05 13:38:24 -0400

    commit inicial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import { saludar } from './js/funciones.js';
 import './css/style.css';
 
-const nombre = 'Migo asda asdasd';
-saludar(nombre);
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/funciones';
+
+export const todoList = new TodoList();
+
+//todoList.todos.forEach( todo => crearTodoHtml (todo) );
+todoList.todos.forEach( crearTodoHtml ); // cuando el argumento retorna un solo elemento y es el que requiere la funcion ( se envia el todo )
+
